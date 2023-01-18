11b3ca5eb8ea157cb25e29c821f04016e1556ad2
Jorge Tapia <jorge504tapia@gmail.com>
2023-01-18 14:11:24 -0300

    actualizacion dbConnect

--- database/db.ts
+++ database/db.ts
@@ -1,11 +1,46 @@
-import { connect, set } from 'mongoose'
+import mongoose from 'mongoose'
 
-export const dbConnect = async () => {
-  try {
-    set('strictQuery', true)
-    const db = await connect(process.env.MONGO_URL!)
-    console.log(db.connection.db.databaseName)
-  } catch (error) {
-    console.error(error)
+const MONGODB_URI = process.env.MONGO_URL
+
+declare global {
+  var mongoose: any
+}
+
+if (!MONGODB_URI) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env.local'
+  )
+}
+
+let cached = global.mongoose
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null }
+}
+
+export async function dbConnect() {
+  mongoose.set('strictQuery', false)
+
+  if (cached.conn) {
+    return cached.conn
   }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    }
+
+    cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
+      return mongoose
+    })
+  }
+
+  try {
+    cached.conn = await cached.promise
+  } catch (e) {
+    cached.promise = null
+    throw e
+  }
+
+  return cached.conn
 }
