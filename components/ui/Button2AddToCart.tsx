3515025c984a0991a6b9c418e808f5e33bdf0816
Jorge Tapia <jorge504tapia@gmail.com>
2023-02-14 13:53:16 -0300

    ver 4

--- components/ui/Button2AddToCart.tsx
+++ components/ui/Button2AddToCart.tsx
@@ -13,20 +13,13 @@ export const Button2AddToCart: React.FC<Props> = ({ tempCartProduct }) => {
 
   const addToCart = () => {
     setText('Producto añadido')
-    try {
-      if (localStorage.getItem('cart')) {
-        const cart = JSON.parse(localStorage.getItem('cart')!)
-        const cartFinal = cart.concat(tempCartProduct)
-        localStorage.setItem('cart', JSON.stringify(cartFinal))
-        setCart(cartFinal)
-      } else {
-        localStorage.setItem('cart', `[${JSON.stringify(tempCartProduct)}]`)
-        setCart(`[${tempCartProduct}]`)
-      }
-    } catch (error) {
-      console.log(error)
+    if (localStorage.getItem('cart')) {
+      const cart = JSON.parse(localStorage.getItem('cart')!)
+      const cartFinal = cart.concat(tempCartProduct)
+      localStorage.setItem('cart', JSON.stringify(cartFinal))
+    } else {
+      localStorage.setItem('cart', `[${JSON.stringify(tempCartProduct)}]`)
     }
-    
     setTimeout(() => {
       setText('Añadir al carrito')
     }, 3000)
