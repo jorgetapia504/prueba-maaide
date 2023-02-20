ee75501bc8c58061fdf81976e9965627cd025cf7
Jorge Tapia <jorge504tapia@gmail.com>
2023-02-20 18:49:12 -0300

    details 2

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -198,13 +198,25 @@ export const Navbar: React.FC<PropsWithChildren<Props>> = ({ children , menu, se
                 setIndex('hidden')
               }, 100)
             }} href='/contacto'>Contacto<AiOutlineRight className='ml-auto text-lg text-neutral-500' /></Link>
+            <div className='flex flex-col gap-2 mt-6'>
+              {
+                categories?.length
+                  ? categories.map(category => (
+                    <Link href={`/tienda/${category.slug}`} className='flex gap-2' key={category._id}>
+                      <img className='w-28' src={category.image} />
+                      <h2 className='mt-auto mb-auto'>{category.category}</h2>
+                    </Link>
+                  ))
+                  : ''
+              }
+            </div>
           </div>
         </div>
         <div className={`${navCategories} absolute top-57 w-full`} onMouseEnter={() => setNavCategories('flex')} onMouseLeave={() => setNavCategories('hidden')}>
           {
             categories?.length
               ? categories.map(category => (
-                <Link href={`/tienda/${category.slug}`} className='w-1/3 bg-cover flex bg-center' style={{ backgroundImage: `url(${category.image})`, height: '400px' }} key={category._id}>
+                <Link href={`/tienda/${category.slug}`} onClick={() => setNavCategories('hidden')} className='w-1/3 bg-cover flex bg-center' style={{ backgroundImage: `url(${category.image})`, height: '400px' }} key={category._id}>
                   <h2 className='m-auto text-center text-white text-4xl font-semibold'>{category.category}</h2>
                 </Link>
               ))
